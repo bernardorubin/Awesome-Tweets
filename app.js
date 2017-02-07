630653a7f8395776c22fe1e8232c96552c59ceac
bernulander <bernulander@gmail.com>
2017-02-06 21:41:39 -0800

    implement 3rd solution with sessions, missing sorting function

--- app.js
+++ app.js
@@ -5,25 +5,38 @@ const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 
 const routes = require('./routes/routes');
 const routesDB = require('./routes/routesDB');
+const routesSession = require('./routes/routesSession');
 
 const app = Express();
+
+// Use this for first 2 routes
+app.use(cookieParser());
+
+// Use this for 3rd router
+// app.use(session({
+//   secret: 'keyboard kitty',
+//   resave: true,
+//   saveUninitialized: true
+// }));
+
 
 app.set('view engine', 'ejs');
 
 app.use(Express.static(path.join(__dirname, 'public')));
 
-app.use(cookieParser());
+
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(logger('dev'));
 
 
 // change this line to use cookies or DB
-app.use('/', routesDB);
+app.use('/', routes);
 
 const PORT = 8080;
 app.listen(PORT, function() { console.log(`Server ON and listening on http://localhost:${PORT}`)});
